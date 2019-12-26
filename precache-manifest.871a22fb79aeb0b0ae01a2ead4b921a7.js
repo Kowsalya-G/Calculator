edb312ad60b1a168377031fb964bf7c838214ef7
Kowsalya G <Kowsalya-6100@csez.zohocorpin.com>
2019-12-26 19:24:56 +0530

    Updates

---
+++ precache-manifest.871a22fb79aeb0b0ae01a2ead4b921a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c72da76fac8fdfc3ddaf1a930f17db",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "fa39bd3ff753f7dedc39",
+    "url": "/Calculator/static/css/main.75ed4552.chunk.css"
+  },
+  {
+    "revision": "54766a654307bffb2e09",
+    "url": "/Calculator/static/js/2.76564e77.chunk.js"
+  },
+  {
+    "revision": "fa39bd3ff753f7dedc39",
+    "url": "/Calculator/static/js/main.a82a1712.chunk.js"
+  },
+  {
+    "revision": "0476f8ab518e3b3f410a",
+    "url": "/Calculator/static/js/runtime-main.48d81177.js"
+  },
+  {
+    "revision": "000a69f4b5996b466c838415a72dd024",
+    "url": "/Calculator/static/media/background.000a69f4.jpg"
+  }
+]);
